dc43544d9925a16da89bf0a1dcbca09eebe0cba7
jokerai0799 <jokerai0799@hotmail.com>
2026-04-18 05:54:12 +0000

    Refresh brand assets from latest pack

--- src/components/brand-logo.tsx
+++ src/components/brand-logo.tsx
@@ -26,8 +26,8 @@ export function BrandLogo({ compact = false, href = '/', variant = 'light', clas
         <Image
           src={logoSrc}
           alt="Quote FollowUp"
-          width={340}
-          height={80}
+          width={360}
+          height={72}
           className="h-10 w-auto sm:h-11"
           priority
         />
